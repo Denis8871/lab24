3c279612978c4768686c2e9a98ac04fd7154968b
Denis8871 <145900110+Denis8871@users.noreply.github.com>
2024-06-16 18:13:39 +0300

    AssemblyInfo.cs

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Lab 24 Dykyi")]
+[assembly: AssemblyTitle("Lab 24 Shostya")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Lab 24 Dykyi")]
+[assembly: AssemblyProduct("Lab 24 Shostya")]
 [assembly: AssemblyCopyright("Copyright ©  2024")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]

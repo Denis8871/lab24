85bc89e1873c5b5fe9457882b86cea6ea73bfb1c
Denis8871 <145900110+Denis8871@users.noreply.github.com>
2024-06-16 18:14:00 +0300

    Resources.Designer.cs

--- Properties/Resources.Designer.cs
+++ Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace Lab_24_Dykyi.Properties
+namespace Lab_24_Shostya.Properties
 {
 
 
@@ -44,7 +44,7 @@ namespace Lab_24_Dykyi.Properties
             {
                 if ((resourceMan == null))
                 {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Lab_24_Dykyi.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Lab_24_Shostya.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
